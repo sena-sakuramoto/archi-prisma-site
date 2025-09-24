11b98fd1227cb3cac685d65a390be53cee39d706
sena-sakuramoto <sena-sakuramoto@users.noreply.github.com>
2025-09-24 20:33:05 +0900

    Adjust Astro config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://sena-sakuramoto.github.io',
+	base: '/archi-prisma-site',
 	vite: {
 		plugins: [tailwindcss()],
 	},
